a9cb3a2ef01312ad1af4acebc3de752e5c8d2672
Luan Rohde <luanrohde11@gmail.com>
2019-08-16 10:49:20 -0300

    Initial Files

--- backend/src/app/models/User.js
+++ backend/src/app/models/User.js
@@ -20,9 +20,15 @@ class User extends Model {
       if (user.password) {
         user.password_hash = await bcrypt.hash(user.password, 8);
       }
-
-      return this;
     });
+    return this;
+  }
+  static associate(models) {
+    this.belongsTo(models.File, { foreignKey: 'avatar_id', as: 'avatar' });
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
   }
 }
 
