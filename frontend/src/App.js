45f72034c352937fc41dc85a95172ad6ace1c1bb
Luan Rohde <luanrohde11@gmail.com>
2019-09-14 16:27:46 -0300

    initial files

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+export default function src() {
+  return (
+    <div className="App">
+      <h1>Helo world</h1>
+    </div>
+  );
+}
