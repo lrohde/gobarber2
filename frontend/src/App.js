2d8e4cbd4ed94c6695a55e2c1e76cbb638827a51
Luan Rohde <luanrohde11@gmail.com>
2019-09-15 11:52:16 -0300

    auth module

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 import './config/ReactotronConfig';
