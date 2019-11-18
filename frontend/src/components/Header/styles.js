5f67bc72f4dd325c5b6a96321fa42ba4ae2f4465
WestSoftware <westsoftware@MacBook-Pro-de-Luan.local>
2019-11-18 20:38:12 -0300

    notification layout

--- frontend/src/components/Header/styles.js
+++ frontend/src/components/Header/styles.js
@@ -38,6 +38,7 @@ export const Content = styled.div`
 export const Profile = styled.div`
   display: flex;
   margin-right: 20px;
+  margin-left: 20px;
   padding-left: 20px;
   border-left: 1px solid #eee;
 
